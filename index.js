dd65f8fdbe8ced7fb077865d3ed6b6b31ca931de
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-01-10 15:39:48 +0100

    Added animation

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ for (var i = 0; i < numberOfDrums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -45,4 +47,12 @@ function makeSound(key) {
     default:
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector(`.${currentKey}`);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
